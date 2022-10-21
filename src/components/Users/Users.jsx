7336c15c165196b4a043f20f3bb3ac8575f38f31
Ivan Savchenko <ivansavchenkogym555@gmail.com>
2022-10-21 10:07:54 +0300

    Lesson 52: Side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,24 @@
+import axios from "axios";
 import React from "react";
+import userPhoto from '../../assets/images/user_icon.png'
 import styles from './Users.module.scss'
 
 const Users = (props) => {
-    debugger
-    if (props.users.length < 1) {
-        props.setUsers([
-            { id: 1, photoUrl: 'https://i.pinimg.com/236x/8e/a3/42/8ea342948281bd784626051d950a9f9f.jpg', followed: true, fullName: 'Ivan', status: '', location: { city: 'Sumy', country: 'Ukraine' } },
-            { id: 2, photoUrl: 'https://i.pinimg.com/236x/8e/a3/42/8ea342948281bd784626051d950a9f9f.jpg', followed: true, fullName: 'Dmitry', status: '', location: { city: 'Sumy', country: 'Ukraine' } },
-            { id: 3, photoUrl: 'https://i.pinimg.com/236x/8e/a3/42/8ea342948281bd784626051d950a9f9f.jpg', followed: true, fullName: 'Kep', status: '', location: { city: 'Sumy', country: 'Ukraine' } },
-        ])
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items );
+            })
+        }
     }
-    debugger
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt='avatar' className={styles.avatar} />
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='avatar' className={styles.avatar} />
                         </div>
                         <div>
                             {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> : <button onClick={() => props.follow(u.id)}>Follow</button>}
@@ -25,12 +26,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)
