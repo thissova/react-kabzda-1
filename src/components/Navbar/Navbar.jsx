1ddc88444a12683b59c54f8d2394be822fcf5f2d
Ivan Savchenko <ivansavchenkogym555@gmail.com>
2023-07-03 13:04:43 +0300

    Add bootstrap to project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,28 +7,31 @@ import groupIcon from '../../assets/images/group.png'
 import newsIcon from '../../assets/images/newsIcon.png'
 import musicIcon from '../../assets/images/music.png'
 import settingsIcon from '../../assets/images/settings.png'
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Navbar = () => {
     return (
+
         <nav className={styles.nav}>
+
+            <Button className={styles.item}><NavLink to="/profile" activeClassName={styles.active}><img src={profileIcon} alt="" /> Profile</NavLink></Button>
+
             <div className={styles.item}>
-                <NavLink to="/profile" activeClassName={styles.active}><img src={profileIcon} alt="" /> Profile</NavLink>
+
+                <Button><NavLink to="/messages" activeClassName={styles.active}> <img src={chatIcon} alt="" /> Messages</NavLink></Button>
             </div>
             <div className={styles.item}>
-                
-                <NavLink to="/messages" activeClassName={styles.active}> <img src={chatIcon} alt="" /> Messages</NavLink>
+                <Button><NavLink to="/users" activeClassName={styles.active}> <img src={groupIcon} alt="" /> Users</NavLink></Button>
             </div>
             <div className={styles.item}>
-                <NavLink to="/users" activeClassName={styles.active}><img src={groupIcon} alt="" />Users</NavLink>
+                <Button><NavLink to="/news" activeClassName={styles.active}> <img src={newsIcon} alt="" /> News</NavLink></Button>
             </div>
             <div className={styles.item}>
-                <NavLink to="/news" activeClassName={styles.active}><img src={newsIcon} alt="" />News</NavLink>
+                <Button><NavLink to="/music" activeClassName={styles.active}> <img src={musicIcon} alt="" /> Music</NavLink></Button>
             </div>
             <div className={styles.item}>
-                <NavLink to="/music" activeClassName={styles.active}><img src={musicIcon} alt="" />Music</NavLink>
-            </div>
-            <div className={styles.item}>
-                <NavLink to="/settings" activeClassName={styles.active}><img src={settingsIcon} alt="" />Settings</NavLink>
+                <Button><NavLink to="/settings" activeClassName={styles.active}> <img src={settingsIcon} alt="" /> Settings</NavLink></Button>
             </div>
 
         </nav>
--- src/data/users-reducer.js
+++ src/data/users-reducer.js
@@ -1,6 +1,6 @@
 let initialState = {
     users: [],
-    pageSize: 10,
+    pageSize: 5,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: false,
